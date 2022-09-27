9b23459c88d9a82b870f576fc9786faf733b3827
hasan <hasan@DESKTOP-1IFBENN>
2022-09-27 13:20:57 +0300

    Methods2

--- Methods2/src/Main.java
+++ Methods2/src/Main.java
@@ -2,8 +2,39 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String mesaj ="Bugun hava cok guzel.";
+		String yeniMesaj = mesaj.substring(0,2);  //substring bir return ediyor.
+		System.out.println(yeniMesaj);
+		String yeniMesaj2 = sehirVer();
+		int sayi = topla(5,7);
+		System.out.println(sayi);
+		
+		
+		
 	}
+	
+	public static void ekle() {
+		System.out.println("Eklendi.");	
+	}
+	
+	public static void sil() {
+		System.out.println("Silindi.");	
+	}
+	
+	public static void guncelle() {
+		System.out.println("Guncellendi.");	
+	}
+	
+	public static int topla(int sayi1,int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	public static String sehirVer() {
+		return "Ankara";
+	}
+	
+	
+	
+	
 
 }
