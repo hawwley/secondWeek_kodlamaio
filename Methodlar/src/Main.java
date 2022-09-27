eac078ab9438d1dceeab6cb950d470ec8b2e7667
hasan <hasan@DESKTOP-1IFBENN>
2022-09-27 12:48:56 +0300

    Methods

--- Methodlar/src/Main.java
+++ Methodlar/src/Main.java
@@ -2,8 +2,33 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		sayiBulmaca(); //Methodu calistiriyor.
+		
+	}
+	
+	//sayiBulmaca methodunu(fonksiyon) olusturuyoruz.
+	public static void sayiBulmaca() {
+		int[] sayilar = new int [] {1,2,5,7,9,0};
+		int aranacak = 0;
+		
+		boolean varMi=false;
+		
+		for(int sayi : sayilar) {
+			if(sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if(varMi){
+			mesajVer("Sayi mevcuttur: " +aranacak);	
+		}else {
+			mesajVer("Sayi mevcut degildir: "+aranacak);	
+		}
+			
+	}
+	
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);		
 	}
 
 }
